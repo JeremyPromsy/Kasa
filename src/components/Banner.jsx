8ad2dc226c617f9789beb5a7acc510b13b57d495
JeremyPromsy <jeremypromsy@gmail.com>
2023-04-26 17:06:27 +0200

    Avancement

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,12 @@
 import "../styles/banner.css"
+import bannerHome from "../assets/BackgroundAccueil.png";
 
 function Banner() {
     return (
         <div className="banner">
-
+                <img className="bannerHome" src= {bannerHome} alt ="bannerHome" /> 
         </div>
     )
 }
 
-export default Banner
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import Banner from "../components/Banner";
-import bannerHome from "../assets/BackgroundAccueil.png";
 import "../styles/home.css"
 
 function Home () {
     return (
         <div className="home">
              <Banner/>
-            <div className="logo">
-                <img className="bannerHome" src= {bannerHome} alt ="bannerHome" /> 
-            </div>
-        
-            
             <h1>Chez vous, partout et ailleurs</h1>
         </div>
     )
