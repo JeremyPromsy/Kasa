0fbc0ece8d1a7cf9e5a47224d4adfeabada5e80b
JeremyPromsy <jeremypromsy@gmail.com>
2023-04-26 17:02:23 +0200

    Maj

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,12 @@ import "../styles/home.css"
 function Home () {
     return (
         <div className="home">
-            <Banner
-            imageHome = {bannerHome}
-            />
+             <Banner/>
+            <div className="logo">
+                <img className="bannerHome" src= {bannerHome} alt ="bannerHome" /> 
+            </div>
+        
+            
             <h1>Chez vous, partout et ailleurs</h1>
         </div>
     )
