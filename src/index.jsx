e8fa118f4efc46799376d0376eb6153d77c19fa1
JeremyPromsy <jeremypromsy@gmail.com>
2023-05-03 17:56:12 +0200

    Avancement Etp 4

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import Footer from './components/Footer'
 
 import Home from './pages/Home'
 import Error404 from './pages/404'
-/*import Fiche from './pages/Fiche'
+/*import Logement from './pages/Logement'
 import APropos from './pages/APropos'*/
 
 import "./index.css"
@@ -19,6 +19,7 @@ root.render(
     <Nav />
     <Routes>
       <Route path = "/" element ={<Home />} />
+      
 
       <Route path = "*" element ={<Error404 />} /> 
     </Routes>
