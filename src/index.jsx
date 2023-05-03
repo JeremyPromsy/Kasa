ddbaa9127395b35d5ce81728fe77f280c42b94fa
JeremyPromsy <jeremypromsy@gmail.com>
2023-05-03 17:10:52 +0200

    Avancement

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Nav from './components/Nav'
-/*import Footer from './components/Footer'*/
+import Footer from './components/Footer'
 
 import Home from './pages/Home'
 import Error404 from './pages/404'
@@ -22,7 +22,7 @@ root.render(
 
       <Route path = "*" element ={<Error404 />} /> 
     </Routes>
-
+    <Footer />
     </BrowserRouter>
   </React.StrictMode>,
 );
