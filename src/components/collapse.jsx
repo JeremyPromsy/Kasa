4c41ace115c263efe5854c5ad19c8cd6c3e44bcb
JeremyPromsy <jeremypromsy@gmail.com>
2023-05-05 17:41:27 +0200

    Avancement

---
+++ src/components/collapse.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import haut from "../assets/FlecheHaut.png";
+import bas from "../assets/FlècheBas.png";
+import "../styles/collapse.css"
+
+function Collapse ( { title, content, open } ) {
+    const [ IsOpen, setIsOpen ] = useState () 
+    return IsOpen?(
+        <div className="collapseFermé">
+            <div className="container">
+                <h3> {title} </h3>
+                <img className="FlècheHaute" src = {haut} alt = "contenu caché" onClick={() => setIsOpen (false)}/>
+            </div>
+            {typeof content === "object" ? affichageValeurs(content) : <p> {content} </p> }
+        </div>
+        ) : (
+        <div className="collapseOuvert">
+            <div className="container">
+                <h3> {title} </h3>
+                <img className="FlècheBasse" src = {bas} alt = "contenu affiché" onClick={() => setIsOpen (true)}/>
+            </div>
+        </div>
+        )
+}
+
+function affichageValeurs (texte) {
+    return (
+        <ul>
+            {texte.map((valeurs) => (
+                <li key= {valeurs}> 
+                </li>
+            ))}
+        </ul> 
+    )
+}
+
+
+export default Collapse ;
