923896a467f87d2f1f96317e0175ae3df4888894
JeremyPromsy <jeremypromsy@gmail.com>
2023-05-17 17:27:14 +0200

    Finalisation Slideshow

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,12 +1,39 @@
 import React, {useState} from "react";
 import flecheDroite from "../assets/FlècheDroite.png";
 import flecheGauche from "../assets/FlècheGauche.png";
-import { useParams, Navigate } from "react-router-dom";
-import annonces from "../annonces/annonces.json"
 import "../styles/slideshow.css"
 
-function Slideshow () {
-    
+function Slideshow({ pictures }) {
+    const [current, setCurrent] = useState(0);
+
+    const nextSlide = () => {
+        setCurrent(current === pictures.length - 1 ? 0 : current + 1);
+    };
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? pictures.length - 1 : current - 1);
+    };
+
+    return (
+        <section id="gallery"> 
+            {pictures.length > 1 && (
+                <img src={flecheDroite} alt="right arrow" onClick={nextSlide}/>
+            )}
+            {pictures.length > 1 && (
+                <img src={flecheGauche} alt="left arrow" onClick={prevSlide}
+                />
+            )}
+
+            {pictures.map((img, index) => {
+                return (
+                    <div key={index}> {index === current && (
+                            <img src={img} alt="Photos du logement" className="gallery-image"/>
+                        )}
+                    </div>
+                );
+            })}
+        </section>
+    );
 }
 
-export default Slideshow ;
+export default Slideshow;
